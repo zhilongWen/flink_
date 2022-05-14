93d8716d5ced73b1159dec1cabc14ab8985214aa
zero <zero@zero.com>
2022-05-14 12:57:01 +0800

    flatMap

--- flink-operator/src/main/java/com/at/_3_flatmap.java
+++ flink-operator/src/main/java/com/at/_3_flatmap.java
@@ -2,6 +2,7 @@ package com.at;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -18,21 +19,39 @@ public class _3_flatmap {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        env.setParallelism(1);
+
         DataStreamSource<Integer> streamSource = env.fromCollection(Arrays.asList(1, 2, 3, 4, 5));
 
-        SingleOutputStreamOperator<Integer> streamOperator = streamSource.flatMap(new FlatMapFunction<Integer, Integer>() {
-            @Override
-            public void flatMap(Integer elem, Collector<Integer> collector) throws Exception {
+        // DataStream → DataStream
+        // flatMap：针对流中的每一个元素，输出零个、一个或多个 元素
+        // flatMap 是 map 与 filter 的泛化  可以实现 map 与 filter 的功能
+//        streamSource.flatMap(new FlatMapFunction<Integer, Integer>() {
+//            @Override
+//            public void flatMap(Integer elem, Collector<Integer> collector) throws Exception {
+//
+//                if (elem % 2 == 1) {
+//                    collector.collect(elem);
+//                    collector.collect(elem);
+//                } else {
+//                    collector.collect(elem + 10);
+//                }
+//            }
+//        }).print();
 
-                if (elem == 1) {
-                    collector.collect(elem);
-                } else {
-                    collector.collect(elem + 10);
-                }
-            }
-        });
 
-        streamOperator.print();
+        streamSource
+                .flatMap((Integer elem, Collector<Integer> collector) -> {
+                    if (elem % 2 == 1) {
+                        collector.collect(elem);
+                        collector.collect(elem);
+                    } else {
+                        collector.collect(elem + 10);
+                    }
+                })
+                .returns(Types.INT)
+                .print();
+
 
         env.execute();
 
