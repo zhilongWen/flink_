3762eb7867335a3febecbaf9aaf61494f99a08ec
zero <zero@zero.com>
2022-05-18 15:28:24 +0800

    window aggregate reduce

---
+++ flink-api/src/main/java/com/at/windowaggrearate/WindowReduce.java
@@ -0,0 +1,43 @@
+package com.at.windowaggrearate;
+
+import com.at.source.ClickSource;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+/**
+ * @create 2022-05-18
+ */
+public class WindowReduce {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        env
+                .addSource(new ClickSource())
+                .map(event -> Tuple2.of(event.user, 1L))
+                .returns(Types.TUPLE(Types.STRING, Types.LONG))
+                .keyBy(elem -> elem.f0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .reduce(
+                        new ReduceFunction<Tuple2<String, Long>>() {
+                            @Override
+                            public Tuple2<String, Long> reduce(Tuple2<String, Long> value1, Tuple2<String, Long> value2) throws Exception {
+                                return Tuple2.of(value1.f0, value1.f1 + value2.f1);
+                            }
+                        }
+                )
+                .print();
+
+
+        env.execute();
+
+
+    }
+
+}
