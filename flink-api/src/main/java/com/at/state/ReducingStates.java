bdbe6fba088121c85d053f387261e47b38e5824d
zero <zero@zero.com>
2022-05-21 01:26:35 +0800

    ReducingStates

---
+++ flink-api/src/main/java/com/at/state/ReducingStates.java
@@ -0,0 +1,23 @@
+package com.at.state;
+
+/**
+ * @create 2022-05-21
+ */
+public class ReducingStates {
+
+    public static void main(String[] args) {
+
+
+        /*
+
+            ReducingState<T>
+                This keeps a single value that represents the aggregation of all values added to the state.
+                The interface is similar to ListState but elements added using add(T) are reduced to an aggregate using a specified ReduceFunction.
+
+
+
+         */
+
+    }
+
+}
