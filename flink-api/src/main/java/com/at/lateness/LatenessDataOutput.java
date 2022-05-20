feb3010f6495004a724454df375b7d8a98663678
zero <zero@zero.com>
2022-05-20 20:31:15 +0800

    迟到数据发送到侧输出流

--- flink-api/src/main/java/com/at/lateness/LatenessDataOutput.java
+++ flink-api/src/main/java/com/at/lateness/LatenessDataOutput.java
@@ -18,7 +18,7 @@ import java.time.Duration;
 /**
  * @create 2022-05-20
  */
-public class OutputLatenessData {
+public class LatenessDataOutput {
 
     public static void main(String[] args) throws Exception {
 
