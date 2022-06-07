51fa3a1ac3bb16b1e02796116bfe66b3cd384aa2
zero <zero@zero.com>
2022-06-07 10:22:23 +0800

    hive init sql

---
+++ flink-connections/src/main/resources/sql/hive-init.sql
@@ -0,0 +1,12 @@
+
+
+-- https://nightlies.apache.org/flink/flink-docs-release-1.15/docs/connectors/table/hive/hive_dialect/
+create catalog myhive with ('type' = 'hive', 'hive-conf-dir' = '/opt/module/hive-3.1.2/conf');
+use catalog myhive;
+load module hive;
+use modules hive,core;
+set table.sql-dialect=hive;
+SET 'execution.runtime-mode' = 'batch';
+
+
+
