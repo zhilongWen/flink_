0ae458bbd76e9e1229a0fd31b9a49a9581d3ed9b
zero <zero@zero.com>
2022-06-16 00:35:03 +0800

    hudi test java.lang.NoSuchMethodError: org.apache.parquet.avro.AvroSchemaConverter.convert(Lorg/apache/hudi/org/apache/avro/Schema;)Lorg/apache/parquet/schema/MessageType;

--- flink-connector-hudi-1.14x/src/main/java/com/at/hudi/KafkaToHudi.java
+++ flink-connector-hudi-1.14x/src/main/java/com/at/hudi/KafkaToHudi.java
@@ -1,5 +1,13 @@
 package com.at.hudi;
 
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.SqlDialect;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.catalog.hive.HiveCatalog;
+
 /**
  * @create 2022-06-15
  */
@@ -7,6 +15,99 @@ public class KafkaToHudi {
 
     public static void main(String[] args) {
 
+
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.enableCheckpointing(3000, CheckpointingMode.EXACTLY_ONCE);
+
+
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        String name = "myhive";
+        String defaultDatabase = "default";
+        String hiveConfDir = "D:\\workspace\\workspace2021\\bigdata-learn\\hive-test\\src\\main\\resources\\conf";
+        String version = "3.1.2";
+
+        HiveCatalog hive = new HiveCatalog(name, defaultDatabase, hiveConfDir, version);
+        tableEnv.registerCatalog("myhive",hive);
+
+
+        tableEnv.useCatalog("myhive");
+        tableEnv.getConfig().setSqlDialect(SqlDialect.HIVE);
+        tableEnv.useDatabase("default");
+
+
+        tableEnv.getConfig().setSqlDialect(SqlDialect.DEFAULT);
+
+        String sourceSQL = "create table if not exists hudi_user_behavior_tbl(\n"
+                + "     userId int,\n"
+                + "     itemId bigint,\n"
+                + "     categoryId int,\n"
+                + "     behavior string,\n"
+                + "     ts bigint,\n"
+                + "     row_time as to_timestamp(from_unixtime(ts / 1000,'yyyy-MM-dd HH:mm:ss')),\n"
+                + "     watermark for row_time as row_time - interval '1' second\n"
+                + ")with(\n"
+                + "    'connector' = 'kafka',\n"
+                + "    'topic' = 'user_behaviors',\n"
+                + "    'properties.bootstrap.servers' = 'hadoop102:9092,hadoop103:9092,hadoop104:9092',\n"
+                + "    'properties.group.id' = 'test-group-id',\n"
+                + "    'scan.startup.mode' = 'earliest-offset',\n"
+                + "    'format' = 'json',\n"
+                + "    'json.ignore-parse-errors' = 'true'\n"
+                + ")";
+
+
+        String hudiSinkSQL = "create table if not exists user_behavior_hms_mor\n"
+                + "(\n"
+                + "    user_id     int,\n"
+                + "    item_id     bigint,\n"
+                + "    category_id int,\n"
+                + "    behavior string,\n"
+                + "    ts          bigint,\n"
+                + "    `dt` string,\n"
+                + "    `hh` string,\n"
+                + "    `mm` string\n"
+                + ")partitioned by (`dt`,`hh`,`mm`)\n"
+                + "with(\n"
+                + "    'connector'='hudi',\n"
+                + "    'path'='hdfs://hadoop102:8020/user/warehouse/user_behavior_hms_mor_tbl',\n"
+                + "    'table.type'='MERGE_ON_READ',    -- MERGE_ON_READ 方式在没生成 parquet 文件前，hive不会有输出\n"
+                + "    'hoodie.datasource.write.recordkey.field' = 'user_id',\n"
+                + "    'write.precombine.field'= 'ts',\n"
+                + "    'write.tasks'='1',\n"
+                + "    'write.rate.limit'= '2000',\n"
+                + "    'compaction.tasks'='1',\n"
+                + "    'compaction.async.enabled'= 'true',\n"
+                + "    'compaction.trigger.strategy'= 'num_and_time',\n"
+                + "    'compaction.delta_commits'= '1',\n"
+                + "    'read.streaming.enabled' = 'true',\n"
+                + "    'read.streaming.check-interval' = '4',\n"
+                + "    'hive_sync.mode' = 'hms',            -- required, 将hive sync mode设置为hms, 默认jdbc\n"
+                + "    'hive_sync.enable'='true',           -- required，开启hive同步功能\n"
+                + "    'hive_sync.metastore.uris' = 'thrift://hadoop102:9083' ,\n"
+                + "    'hive_sync.table'='user_behavior_hms_mor_tbl',                          -- required, hive 新建的表名\n"
+                + "    'hive_sync.db'='default',                       -- required, hive 新建的数据库名\n"
+                + "    'hive_sync.support_timestamp'= 'true'\n"
+                + ")";
+
+
+        String insertSQL = "insert into user_behavior_hms_mor\n"
+                + "select\n"
+                + "    userId as user_id,\n"
+                + "    itemId as item_id,\n"
+                + "    categoryId as category_id,\n"
+                + "    behavior,\n"
+                + "    ts,\n"
+                + "    date_format(cast(row_time as string),'yyyyMMdd') dt,\n"
+                + "    date_format(cast(row_time as string),'HH') hh,\n"
+                + "    date_format(cast(row_time as string),'mm') mm\n"
+                + "from hudi_user_behavior_tbl";
+
+        tableEnv.executeSql(sourceSQL);
+        tableEnv.executeSql(hudiSinkSQL);
+        tableEnv.executeSql(insertSQL);
 
 
     }
