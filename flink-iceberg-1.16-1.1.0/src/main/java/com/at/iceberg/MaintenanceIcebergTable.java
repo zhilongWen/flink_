3b19b140ba219703e30f6837b65d0cd148020af6
wenzhilong <wenzhilong@wenzhilongdeMacBook-Pro.local>
2025-06-28 18:38:32 +0800

    feat: flink iceberg 表管理

---
+++ flink-iceberg-1.16-1.1.0/src/main/java/com/at/iceberg/MaintenanceIcebergTable.java
@@ -0,0 +1,35 @@
+package com.at.iceberg;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.flink.actions.Actions;
+import org.apache.iceberg.hadoop.HadoopCatalog;
+
+public class MaintenanceIcebergTable {
+    public static void main(String[] args) {
+
+        System.setProperty("HADOOP_USER_NAME", "root");
+
+        // 1.获取 Table对象
+        // 1.1 创建 catalog对象
+        Configuration conf = new Configuration();
+        HadoopCatalog hadoopCatalog = new HadoopCatalog(
+                conf,
+                "hdfs://10.211.55.102:8020/user/hive/warehouse/iceberg_db.db/iceberg_hadoop"
+        );
+
+        // 1.2 通过 catalog加载 Table对象
+        Table table = hadoopCatalog.loadTable(TableIdentifier.of("default", "user_behavior_flink_2"));
+
+        // 有Table对象，就可以获取元数据、进行维护表的操作
+//        System.out.println(table.history());
+//        System.out.println(table.expireSnapshots().expireOlderThan());
+
+        // 2.通过 Actions 来操作 合并
+        Actions.forTable(table)
+                .rewriteDataFiles()
+                .targetSizeInBytes(1024L)
+                .execute();
+    }
+}
