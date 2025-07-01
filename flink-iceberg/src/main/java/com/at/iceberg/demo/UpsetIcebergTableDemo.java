7fb96f8a23fb0efd8ca3cca4ad7d07bd3789a8e9
wenzhilong <wenzhilong@wenzhilongdeMacBook-Pro.local>
2025-07-01 22:38:00 +0800

    feat: flink sql upset demo 1

---
+++ flink-iceberg/src/main/java/com/at/iceberg/demo/UpsetIcebergTableDemo.java
@@ -0,0 +1,89 @@
+package com.at.iceberg.demo;
+
+import org.apache.flink.configuration.CheckpointingOptions;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.StateBackendOptions;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+public class UpsetIcebergTableDemo {
+    public static void main(String[] args) throws Exception {
+
+        System.setProperty("HADOOP_USER_NAME", "root");
+
+        Configuration defaultConfig = new Configuration();
+        defaultConfig.setString("rest.bind-port", "8081");
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(defaultConfig);
+        EnvironmentSettings settings = EnvironmentSettings.newInstance().inStreamingMode().build();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
+
+        env.setRestartStrategy(
+                org.apache.flink.api.common.restartstrategy.RestartStrategies.fixedDelayRestart(
+                        3,
+                        // 10 seconds restart window
+                        10000
+                )
+        );
+
+        // start a checkpoint every 1000 ms
+        env.enableCheckpointing(1000);
+        // advanced options:
+        // set mode to exactly-once (this is the default)
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        // make sure 500 ms of progress happen between checkpoints
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(60 * 1000);
+        // checkpoints have to complete within one minute, or are discarded
+        env.getCheckpointConfig().setCheckpointTimeout(60 * 1000);
+        // only two consecutive checkpoint failures are tolerated
+        env.getCheckpointConfig().setTolerableCheckpointFailureNumber(3);
+        // allow only one checkpoint to be in progress at the same time
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        // enable externalized checkpoints which are retained
+        // after job cancellation
+        env.getCheckpointConfig().setExternalizedCheckpointCleanup(
+                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        // enables the unaligned checkpoints
+//        env.getCheckpointConfig().enableUnalignedCheckpoints();
+        // sets the checkpoint storage where checkpoint snapshots will be written
+        Configuration checkpointConfig = new Configuration();
+//        checkpointConfig.set(StateBackendOptions.STATE_BACKEND, "rocksdb");
+//        checkpointConfig.set(CheckpointingOptions.CHECKPOINT_STORAGE, "filesystem");
+//        checkpointConfig.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, "hdfs://127.0.0.1:8020/tmp/checkpoints");
+        checkpointConfig.set(StateBackendOptions.STATE_BACKEND, "hashmap");
+        checkpointConfig.set(CheckpointingOptions.CHECKPOINT_STORAGE, "filesystem");
+        checkpointConfig.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, "file:///Users/wenzhilong/warehouse/space/flink_/flink-iceberg/checkpoints");
+        env.configure(checkpointConfig);
+
+
+        tableEnv.executeSql("create catalog hadoop_catalog with (\n"
+                + "  'type'='iceberg',\n"
+                + "  'catalog-type'='hadoop',\n"
+                + "  'warehouse'='hdfs://10.211.55.102:8020/user/hive/warehouse/iceberg_db.db/iceberg_hadoop',\n"
+                + "  'property-version'='1'\n"
+                + ")");
+
+        tableEnv.executeSql("use catalog hadoop_catalog");
+
+        tableEnv.executeSql("show tables").print();
+
+//        tableEnv.executeSql("CREATE TABLE if not exists sample_3 (\n"
+//                + "  id INT UNIQUE COMMENT 'unique id',\n"
+//                + "  data STRING NOT NULL,\n"
+//                + "  PRIMARY KEY(id) NOT ENFORCED\n"
+//                + ") with (\n"
+//                + "  'format-version'='2', \n"
+//                + "  'write.upsert.enabled'='true'\n"
+//                + ")");
+//
+//        tableEnv.executeSql("INSERT INTO sample_3 VALUES (1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'), (5, 'e')");
+
+//        tableEnv.executeSql("SELECT * FROM sample_3").print();
+
+//        tableEnv.executeSql("INSERT INTO sample_3 VALUES (1, 'c')");
+
+        tableEnv.executeSql("SELECT * FROM sample_3").print();
+    }
+}
